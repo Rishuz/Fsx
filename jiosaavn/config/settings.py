7aeede6a5c3edc4456df0347ff19777f6f3678ad
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-12 08:41:51 +0530

    Update settings.py

--- jiosaavn/config/settings.py
+++ jiosaavn/config/settings.py
@@ -1,13 +1,13 @@
 from os import getenv
 
-API_ID = getenv("API_ID")
-API_HASH = getenv("API_HASH")
+API_ID = getenv("API_ID","14050586")
+API_HASH = getenv("API_HASH","42a60d9c657b106370c79bb0a8ac560c")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_COMMANDS = (
-    ("start", "Initialize the bot and check its status"),
-    ("settings", "Configure and manage bot settings"),
-    ("help", "Get information on how to use the bot"),
-    ("about", "Learn more about the bot and its features"),
+    ("start", "侖 sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ 侖"),
+    ("settings", "侖 ᴏᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢ 侖"),
+    ("help", "侖 ɢᴇᴛ ʙᴏᴛ ʜᴇʟᴘ 侖"),
+    ("about", "侖 ᴋɴᴏᴡ ᴀʙᴏᴜᴛ ᴛʜᴇ ʙᴏᴛ 侖"),
 )
 
 DATABASE_URL = getenv("DATABASE_URL", None)
